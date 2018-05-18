54868a555b95b277bf7e91e6fc140f3800eb342e
jaminamitani <39251793+jaminamitani@users.noreply.github.com>
2018-05-18 09:53:55 +0900

    Add files via upload
    codes up to  part 1

---
+++ MinecraftSimulation/src/minecraftsimulation/FPCameraController.java
@@ -0,0 +1,213 @@
+
+package minecraftsimulation;
+
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glRotatef;
+import static org.lwjgl.opengl.GL11.glTranslatef;
+import static org.lwjgl.opengl.GL11.glVertex3f;
+import org.lwjgl.util.vector.Vector3f;
+
+public class FPCameraController {
+    
+    private Vector3f position = null;
+    private Vector3f lPosition = null;
+    private float yaw = 0f;
+    private float pitch = 0f;
+    private Vector3f camera;
+    
+    public FPCameraController(float x, float y, float z) { 
+        //instantiate position Vector3f to the x y z params.
+        position = new Vector3f(x, y, z);
+        lPosition = new Vector3f(x,y,z);
+        lPosition.x = 0f;
+        lPosition.y = 15f;
+        lPosition.z = 0f;
+    }
+
+   //increment the camera's current yaw rotation
+   public void yaw(float amount) {
+       //increment the yaw by the amount param
+       yaw += amount;
+   }
+    //increment the camera's current yaw rotation
+    public void pitch(float amount) {
+        //increment the pitch by the amount param
+        pitch -= amount;
+    }
+    //moves the camera forward relative to its current rotation (yaw)
+    public void walkForward(float distance) {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw));
+        position.x -= xOffset;
+        position.z += zOffset;
+    }
+
+    //moves the camera backward relative to its current rotation (yaw)
+    public void walkBackwards(float distance) {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw));
+        position.x += xOffset;
+        position.z -= zOffset;
+    }
+
+    //strafes the camera left relative to its current rotation (yaw)
+    public void strafeLeft(float distance) {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw-90));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw-90));
+        position.x -= xOffset;
+        position.z += zOffset;
+    }
+
+    //strafes the camera right relative to its current rotation (yaw)
+    public void strafeRight(float distance) {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw+90));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw+90));
+        position.x -= xOffset;
+        position.z += zOffset;
+    }
+    //moves the camera up relative to its current rotation (yaw)
+    public void moveUp(float distance) {
+        position.y -= distance;
+    }
+    //moves the camera down
+    public void moveDown(float distance) {
+        position.y += distance;
+    }
+
+    //translates and rotate the matrix so that it looks through the camera
+    //this does basically what gluLookAt() does
+    public void lookThrough() {
+        //roatate the pitch around the X axis
+        glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+        //roatate the yaw around the Y axis
+        glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+        //translate to the position vector's location
+        glTranslatef(position.x, position.y, position.z);
+    }
+    
+    // keep the game session until user presses ESC
+    public void gameLoop() {
+        FPCameraController camera = new FPCameraController(0, 0, 0);
+        float dx = 0.0f;
+        float dy = 0.0f;
+        float dt = 0.0f; //length of frame
+        float lastTime = 0.0f; // when the last frame was
+        long time = 0;
+        float mouseSensitivity = 0.09f;
+        float movementSpeed = .35f;
+        //hide the mouse
+        Mouse.setGrabbed(true); 
+        // keep looping till the display window is closed the ESC key is down
+        while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+            time = Sys.getTime();
+            lastTime = time;
+            //distance in mouse movement
+            //from the last getDX() call.
+            dx = Mouse.getDX();
+            //distance in mouse movement
+            //from the last getDY() call.
+            dy = Mouse.getDY();
+            //when passing in the distance to move
+            //we times the movementSpeed with dt this is a time scale
+            //so if its a slow frame u move more then a fast frame
+            //so on a slow computer you move just as fast as on a fast computer
+            camera.yaw(dx * mouseSensitivity);
+            camera.pitch(dy * mouseSensitivity);
+            if (Keyboard.isKeyDown(Keyboard.KEY_W))//move forward          
+                camera.walkForward(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_S))//move backward            
+                camera.walkBackwards(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_A))//strafe left            
+                camera.strafeLeft(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_D))//strafe right            
+                camera.strafeRight(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_SPACE))//move up            
+                camera.moveUp(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))  
+                camera.moveDown(movementSpeed);               
+            if (Keyboard.isKeyDown(Keyboard.KEY_E)) // for screenshot purposes
+                Mouse.setGrabbed(false);     
+            if (Keyboard.isKeyDown(Keyboard.KEY_R))  
+                Mouse.setGrabbed(true);  
+            //set the modelview matrix back to the identity
+            glLoadIdentity();
+            //look through the camera before you draw anything
+            camera.lookThrough();
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            //you would draw your scene here.
+            render();
+            //draw the buffer to the screen
+            Display.update();
+            Display.sync(60);
+        }
+        Display.destroy();
+    }
+
+    // draw the cube
+    private void render() {
+        // Left
+        glColor3f(0f, 1f, 0f);
+        glBegin(GL_QUADS);
+        glVertex3f(-1f, 1f, 1f);
+        glVertex3f(-1f, 1f, -1f);
+        glVertex3f(-1f, -1f, -1f);
+        glVertex3f(-1f, -1f, 1f);
+        glEnd();
+        
+        // Right
+        glColor3f(0f, 1f, 1f);
+        glBegin(GL_QUADS);
+        glVertex3f(1f, 1f, -1f);
+        glVertex3f(1f, 1f, 1f);
+        glVertex3f(1f, -1f, 1f);
+        glVertex3f(1f, -1f, -1f);
+        glEnd();
+        
+        // Top
+        glColor3f(1f, 0f, 1f);
+        glBegin(GL_QUADS);
+        glVertex3f(1f, 1f, -1f);
+        glVertex3f(-1f, 1f, -1f);
+        glVertex3f(-1f, 1f, 1f);
+        glVertex3f(1f, 1f, 1f);
+        glEnd();
+        
+        // Bottom
+        glColor3f(1f, 1f, 0f);
+        glBegin(GL_QUADS);
+        glVertex3f(1f, -1f, 1f);
+        glVertex3f(-1f, -1f, 1f);
+        glVertex3f(-1f, -1f, -1f);
+        glVertex3f(1f, -1f, -1f);
+        glEnd();
+        
+        // Front
+        glColor3f(1f, 0f, 0f);
+        glBegin(GL_QUADS);
+        glVertex3f(1f, 1f, 1f);
+        glVertex3f(-1f, 1f, 1f);
+        glVertex3f(-1f, -1f, 1f);
+        glVertex3f(1f, -1f, 1f);
+        glEnd();
+        
+        // Back
+        glColor3f(0f, 0f, 1f);
+        glBegin(GL_QUADS);
+        glVertex3f(1f, -1f, -1f);
+        glVertex3f(-1f, -1f, -1f);
+        glVertex3f(-1f, 1f, -1f);
+        glVertex3f(1f, 1f, -1f);
+        glEnd();   
+    }
+}
---
+++ MinecraftSimulation/src/minecraftsimulation/MinecraftSimulation.java
@@ -0,0 +1,67 @@
+/***********************************
+*   file: MinecraftSimulation.java
+*   author: Justen Minamitani, Matthew Haddad, Kathleen Phan, Junda Lou, Robbie Blanco
+*   class: CS445.01 - Computer Graphics
+*   assignment: Final Project - Minecraft Simulation
+*   date last modified: 5/6/2018
+*   purpose: Simulate a scene in the Minecraft
+***********************************/
+package minecraftsimulation;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_NICEST;
+import static org.lwjgl.opengl.GL11.GL_PERSPECTIVE_CORRECTION_HINT;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glHint;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import org.lwjgl.util.glu.GLU;
+
+
+public class MinecraftSimulation {
+    
+    public static void initGL(DisplayMode displayMode) {
+        glClearColor(0f, 0f, 0f, 0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        GLU.gluPerspective(100f, (float)displayMode.getWidth() / (float)displayMode.getHeight(), 0.1f, 300f);
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    }
+
+    public static DisplayMode createWindow() throws Exception {
+        Display.setFullscreen(false);
+        DisplayMode[] dMode = Display.getAvailableDisplayModes();
+        DisplayMode displayMode = null;
+        for(int i = 0; i < dMode.length; i++) {
+            if(dMode[i].getWidth() == 640 && dMode[i].getHeight() == 480 && dMode[i].getBitsPerPixel() == 32) {
+                displayMode = dMode[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("Final Project");
+        Display.create();
+        return displayMode;
+    }
+    
+    public static void main(String[] args) {
+        FPCameraController fp = new FPCameraController(0f, 0f, 0f);
+        DisplayMode displayMode = null;
+        try {
+            displayMode = createWindow();
+            initGL(displayMode);
+            glEnable(GL_DEPTH_TEST);
+            fp.gameLoop();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
